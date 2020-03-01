e55b7c61167345953c64afbc61c75b7ae8c35a45
Johnson <ianbjohn@iu.edu>
2020-02-29 20:38:50 -0500

    More boilerplate progress.

--- organize_data.py
+++ organize_data.py
@@ -1,6 +1,9 @@
 #Quick test code to organize our csv data a bit better
 
+#! /usr/bin/python3
+
 import sys
+from io import open  #Seems hacky, but test without it, the machine I'm currently working on might just have python2 installed or something
 import numpy as np
 
 
@@ -10,15 +13,19 @@ def main():
         return -1;
 
     try:
-        file = open(sys.argv[1], "r", "utf-8")
-    except IOError, e:
-        print(e.strerr())
+        file = open(sys.argv[1], mode = "r", encoding = "utf-8")
+    except IOError as e:
+        print(e)
         return -1;
+        
+    print("File Opened.\n");
         
     #We'll assume that the user is smart enough to have typed a .csv file. Maybe add error handling eventually
     
     #set up our array
     data_array = np.array([12, 31])
+    
+    file.close()
     
     return 0
     
